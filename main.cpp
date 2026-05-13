51c2c3e72ce32844bc24d839f3f97f686f8ff61d
Audringa Daskis <audringadaskis@gmail.com>
2026-05-13 17:03:01 +0300

    Deleted comment

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 
-// sienos (su grindimis, floor levels), priesai ir judejimas, +collision
 int main() {
     sf::RenderWindow window(sf::VideoMode({800, 600}, 32), "The Wormhole");
     sf::Clock clock;
